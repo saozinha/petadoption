52994aba560cb3e977cadd78ec88764cd2474abb
Olavo Shibata <olavoshibata@gmail.com>
2016-04-09 02:01:49 -0300

    inclusão de layout padrão para o manager

---
+++ controllers/dashboard.js
@@ -0,0 +1,15 @@
+var User = require('./../models/user');
+
+var DashboardController = {
+  index: function(req, res) {
+    User.find({ flActive: true }, function(err, users) {
+      if (err)
+        res.send(err);
+
+      //res.json(users);
+      res.render('dashboard/index', { users: users });
+    });
+  },
+};
+
+module.exports = DashboardController;
