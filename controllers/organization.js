b088b613ee28d3b49a4b4f427a95769e05d27731
Olavo Shibata <olavoshibata@gmail.com>
2016-06-03 23:00:53 -0300

    erro no teste

--- controllers/organization.js
+++ controllers/organization.js
@@ -6,26 +6,10 @@ var OrganizationController = {
   // index: function(req, res) {
   // },
   show: (req, res) => {
-    Promise.all([
-      User.findById(req.user._id).execAsync()
-      .then((user) => {
-        return user;
-      })
-      .catch((err) => {
-        throw err;
-      }),
-      Organization.findById(req.params.id).execAsync()
-      .then((org) => {
-        return org;
-      })
-      .catch((err) => {
-        throw err;
-      })
-    ])
-    .then(results => {
-        // the results array will equal ['one','two'] even though
-        // the second function had a shorter timeout.
-      res.render('org/show', { user: results[0], org: results[1] });
+    Organization.findById(req.params.id).execAsync()
+    .then((org) => {
+      return org;
+      res.render('org/show', { userActive: req.user, org: org });
     })
     .catch(err => { 
       console.log(err);
@@ -48,28 +32,13 @@ var OrganizationController = {
     });
   },
   update: function(req, res) {
-    Promise.all([
-      User.findById(req.user._id).execAsync()
-      .then((user) => {
-        return user;
-      })
-      .catch((err) => {
-        throw err;
-      }),
-      Organization.findById(req.body._id).execAsync()
-      .then((org) => {
-        for (var key in req.body) {
-          org[key] = req.body[key];
-        }
+    Organization.findById(req.body._id).execAsync()
+    .then((org) => {
+      for (var key in req.body) {
+        org[key] = req.body[key];
+      }
 
-        return org.saveAsync();
-      })
-      .catch((err) => {
-        throw err;
-      })
-    ])
-    .then(results => {
-      res.render('org/show', { user: results[0], org: results[1] });
+      res.render('org/show', { userActive: req.user, org: org });
     })
     .catch(err => { 
       console.log(err);
